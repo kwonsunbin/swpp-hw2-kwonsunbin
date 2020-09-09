70acda48fba4e44dc1bb95d346d129344638eb83
권순빈 <sunbin1696@gmail.com>
2020-09-10 01:00:23 +0900

    20200910

--- app.js
+++ app.js
@@ -11,37 +11,56 @@ let records = [];
 //     e.g. records: [{year: 2001, rank: 1, name: "Jacob", gender: "M", rankChange: null},
 //                    {year: 2001, rank: 2, name: "Michael", gender: "M", rankChange: null},
 //                    ...]
-// Note: a CSV text can end with trailing line-break character '\n'. Whether it exists or not, 
+// Note: a CSV text can end with trailing line-break character '\n'. Whether it exists or not,
 // the function should parse `text` correctly. Also, records should be stored in the same order
 // in which they appear in a csv text. You can assume that at the first line is always a csv header.
 function parseAndSave(text) {
-    // TODO: Fill this function. (3 points)
-
+  text.split('\n').map((line) => {
+    line = line.trim();
+    var record = {
+      year: parseInt(line.split(',')[0]),
+      rank: parseInt(line.split(',')[1]),
+      name: line.split(',')[2],
+      gender: line.split(',')[3],
+      rankChange: line.split(',')[4] === '' ? null : line.split(',')[4],
+    };
+    if (record.gender !== 'gender' && record.gender !== undefined) {
+      records.push(record);
+    }
+  });
+  console.log(records);
 }
-
 
 // `provideYearData(year)` is a function that receives a year and returns an array of data object corresponding to that year.
 // Note that male and female record with the same rank should be joined together to form one object.
 // TODO: return all data objects of a specific year, that are joined and organized by rank in an ascending order.
 // The example of returned array is as follows.
 //     e.g. [{rank: 1, male: "Jacob", maleRankChange: 0, female: "Isabella", femaleRankChange: 0},
-//           {rank: 2, male: "Ethan", maleRankChange: 0, female: "Sophia", femaleRankChange: -2},    
+//           {rank: 2, male: "Ethan", maleRankChange: 0, female: "Sophia", femaleRankChange: -2},
 //           ...,
 //           {rank: 1000, male: "Keshawn", maleRankChange: 113, female: "Karley", femaleRankChange: 17}]
 function provideYearData(year) {
-    // TODO: Fill in this function. (5 points)
-
-    // This is just a reference for the return value's format. Delete this and fill your own 
-    // proper code to return the correct data.
-    return [
-        {
-            rank: 1,
-            male: "John",
-            maleRankChange: 0,
-            female: "Christina",
-            femaleRankChange: -2
-        }
-    ];
+  // TODO: Fill in this function. (5 points)
+  let = yearData = [];
+  for (var i = 1; i < 1001; i++) {
+    yearData.push({ rank: i });
+  }
+  // This is just a reference for the return value's format. Delete this and fill your own
+  // proper code to return the correct data.
+  records.map((record) => {
+    if (record.year === year) {
+      if (record.gender === 'M') {
+        yearData[record.rank - 1].male = record.name;
+        yearData[record.rank - 1].maleRankChange = record.rankChange;
+      }
+      if (record.gender === 'F') {
+        yearData[record.rank - 1].female = record.name;
+        yearData[record.rank - 1].femaleRankChange = record.rankChange;
+      }
+    }
+  });
+  console.log(yearData);
+  return yearData;
 }
 
 // provideChartData(name, gender) is a function called when a user wants
@@ -60,40 +79,98 @@ function provideYearData(year) {
 //           ...,
 //           {year: 2018, rank: 380}]
 function provideChartData(name, gender) {
-    // TODO: Fill in this function. (2 points)
+  // TODO: Fill in this function. (2 points)
+  let chartData = [];
 
-    // This is just a reference for the return value's format. Delete this and fill your own 
-    // proper code to return the correct data.
-    return [{year: 2001, rank: 3}, {year: 2002, rank: undefined}];
+  records
+    .filter((record) => record.name === name && record.gender === gender)
+    .map((record) => {
+      chartData.push({ year: record.year, rank: record.rank });
+    });
+  // This is just a reference for the return value's format. Delete this and fill your own
+  // proper code to return the correct data.
+  console.log(chartData);
+  return chartData;
 }
-
 
 // `handleSignUpFormSubmit(form)` is called when a user submits the sign up form.
 // `form` is the target HTML form element (L82~ in index.html).
 // TODO: validate the form. (5 points)
 function handleSignUpFormSubmit(form) {
-    let alertMessage = 'TODO: Fill in this alert message properly';
-    // TODO: Fill in the rest of function to get the HTML form element as above.
+  let alertMessage = 'Successfully Submitted!';
+  let valid1 = true;
+  let valid2 = true;
+  let valid3 = true;
+  let valid4 = true;
+  let message1 = null;
+  let message2 = null;
+  let message3 = null;
+  letmessage4 = null;
+  if (!form['first-name'].value.match(/^[A-Z][a-z]+$/)) {
+    message1 = 'Invalid first name';
+    valid1 = false;
+  }
 
-    // Hint: you can use the `RegExp` class for matching a string.
+  if (!form['last-name'].value.match(/^[A-Z][a-z]+$/)) {
+    message2 = 'Invalid last name';
+    valid2 = false;
+  }
 
-    // The return data format is as follows. For the given `form` argument, you should
-    // correctly process the validation, filling in `alertMessage`, and `validationResults`. 
-    // When you deal with `validationResults`, the values of `message` should be set to `null`
-    // for the valid input fields. (See the example below.)
-    // Below is just a reference for the return value's format. Delete this and fill your own
-    // proper code to return the correct data.
+  if (!form['email'].value.match(/^[^@\s]+@[^@\s\.]+\.[a-zA-Z]{2,3}$/)) {
+    message3 = 'Invalid email';
+    valid3 = false;
+  }
 
-    // IMPORTANT NOTE: You must use the argument `form` rather than directly using APIs such as `document.getElementId` or `document.querySelector`.
-    //                 Plus, please do not call `alert` function here.
-    //                 For debugging purpose, you can use `console.log`.
-    return {
-        alertMessage: alertMessage, 
-        validationResults: [
-            {name: "first-name", valid: true, message: null},
-            {name: "last-name", valid: false, message: "Invalid last name"},
-            {name: "email", valid: true, message: null},
-            {name: "date-of-birth", valid: false, message: "Invalid date of birth"}
-        ]
-    };
+  if (
+    !form['date-of-birth'].value.match(/^\d{4}-\d{2}-\d{2}$/) ||
+    parseInt(form['date-of-birth'].value.split('-')[0]) > 2020 ||
+    parseInt(form['date-of-birth'].value.split('-')[0]) < 1900 ||
+    parseInt(form['date-of-birth'].value.split('-')[1]) > 13 ||
+    parseInt(form['date-of-birth'].value.split('-')[1]) < 1 ||
+    parseInt(form['date-of-birth'].value.split('-')[2]) < 1 ||
+    parseInt(form['date-of-birth'].value.split('-')[2]) > 31
+  ) {
+    message4 = 'Invalid date of birth';
+    valid4 = false;
+  }
+
+  if (!valid1 || !valid2 || !valid3 || !valid4) {
+    alertMessage = 'You must correct:\n\n';
+  }
+  if (!valid1) {
+    alertMessage = alertMessage + 'First Name\n';
+  }
+  if (!valid2) {
+    alertMessage = alertMessage + 'Last Name\n';
+  }
+  if (!valid3) {
+    alertMessage = alertMessage + 'Email\n';
+  }
+  if (!valid4) {
+    alertMessage = alertMessage + 'Date of Birth\n';
+  }
+  // TODO: Fill in the rest of function to get the HTML form element as above.
+
+  // Hint: you can use the `RegExp` class for matching a string.
+
+  // The return data format is as follows. For the given `form` argument, you should
+  // correctly process the validation, filling in `alertMessage`, and `validationResults`.
+  // When you deal with `validationResults`, the values of `message` should be set to `null`
+  // for the valid input fields. (See the example below.)
+  // Below is just a reference for the return value's format. Delete this and fill your own
+  // proper code to return the correct data.
+
+  // IMPORTANT NOTE: You must use the argument `form` rather than directly using APIs such as `document.getElementId` or `document.querySelector`.
+  //                 Plus, please do not call `alert` function here.
+  //                 For debugging purpose, you can use `console.log`.
+
+  return {
+    alertMessage: alertMessage,
+    validationResults: [
+      { name: 'first-name', valid: valid1, message: message1 },
+      { name: 'last-name', valid: valid2, message: message2 },
+      { name: 'email', valid: valid3, message: message3 },
+      { name: 'date-of-birth', valid: valid4, message: message4 },
+    ],
+  };
 }
